ce2da313d66b480526e2685f2de0cff5660c1660
Işılay Gümüş <isilaygumus@gmail.com>
2026-04-04 12:48:53 +0300

    Update fonk.js

--- frontend/fonk.js
+++ frontend/fonk.js
@@ -13,3 +13,32 @@ function go_to_home() {
 function go_to_koltuk() {
     window.location.href = "koltuk.html"
 }
+
+// Kayıt Fonksiyonu
+function register() {
+    fetch("../backend/register.php?action=register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            email: document.getElementById('uyeemail').value,
+            name: document.getElementById('uyename').value,
+            password: document.getElementById('uyepassword').value
+        })
+    })
+    .then(res => res.json())
+    .then(data => alert(data.status));
+}
+
+// Giriş Fonksiyonu
+function login() {
+    fetch("../backend/register.php?action=login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            email: document.getElementById('email').value,
+            password: document.getElementById('password').value
+        })
+    })
+    .then(res => res.json())
+    .then(data => alert(data.status));
+}
